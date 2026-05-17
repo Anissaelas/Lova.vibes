073f36d022499bfacca4ee7bc65f733c3fcf7a46
anissa <anissa.el.asri@student.uva.nl>
2026-05-17 18:00:56 +0200

    Initialize Firebase in firebase.js
    Added Firebase initialization with configuration.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJRuv4-dudOel0FkwjYJJ0O5TuUQH2Rpg",
+  authDomain: "locavibes-24dce.firebaseapp.com",
+  projectId: "locavibes-24dce",
+  storageBucket: "locavibes-24dce.firebasestorage.app",
+  messagingSenderId: "514346232936",
+  appId: "1:514346232936:web:39ab7ed0d0a5f7f0f15fdd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
